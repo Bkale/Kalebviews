4478c070e2f2ba5c2e97a2bbb8aee7419e253d3f
Babajide Kale <bkale09@gmail.com>
2016-09-14 15:56:41 -0400

    home/about

--- kalebviews/src/containers/DevPortfolio.js
+++ kalebviews/src/containers/DevPortfolio.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react'
 import '../css/DevPortfolio.css'
 
 class DevPortfolio extends Component {
+
+
+
   render() {
     return(
-      <div className="DevPortfolioBody">Dev Portfolio</div>
+      <div className="DevPortfolioBody">
+        <div>
+          <h2>DrinksOnUs</h2>
+          <p>I developed a 4 page web application that uses the absolute api to render drinks and instructions on how to make them. Included in the instruction page is a video embbed in a modal.</p>
+        </div>
+        <div className="project1"/>
+      </div>
     )
   }
 }
--- kalebviews/src/containers/Home.js
+++ kalebviews/src/containers/Home.js
@@ -1,10 +1,47 @@
 import React, { Component } from 'react'
+import FontAwesome from 'react-fontawesome'
+import '../font-awesome/css/font-awesome.css'
 import '../css/Home.css'
 
 class Home extends Component {
+
+
   render() {
     return(
-      <div className="HomeBody">Home Here</div>
+      <div className="HomeBody">
+        <div className="Content">
+          <h2>HI THERE, WELCOME TO MY SITE</h2>
+          <p>LET'S TAKE A LOOK AROUND...</p>
+
+          <div className="logos">
+          <a href="mailto:bkale09@gmail.com?Subject=Hi">
+            <FontAwesome
+              className='super-crazy-colors'
+              name='envelope-o'
+              size='2x'
+              spin
+              style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
+            />
+          </a>
+          <a href="https://www.linkedin.com/in/babajidekale">
+            <FontAwesome
+              className='super-crazy-colors'
+              name='linkedin'
+              size='2x'
+              spin
+              style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
+            /></a>
+          <a href="https://github.com/Bkale">
+            <FontAwesome
+              className='super-crazy-colors'
+              name='github'
+              size='2x'
+              spin
+              style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
+            /></a>
+          </div>
+       </div>
+      </div>
     )
   }
 }
